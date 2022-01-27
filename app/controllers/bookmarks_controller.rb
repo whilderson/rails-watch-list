8b011e60656c9364f706e591e83aa4b4e4d3bdfa
Wouter Hilderson <wouter.hilderson@gmail.com>
2022-01-27 21:07:47 +0100

    simplified new_bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,13 @@
 class BookmarksController < ApplicationController
+  before_action :set_list
+
   def new
-    @movies = Movie.all
-    @list = List.find(params[:list_id])
+    @movies = Movie.all.order(:title)
     @bookmark = Bookmark.new
   end
 
   def create
-    @movie = Movie.find(params[:bookmark][:movie])
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.movie = @movie
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
@@ -20,7 +18,11 @@ class BookmarksController < ApplicationController
 
   private
 
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
